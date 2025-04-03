90aee4ec0d005225665dab60b2eaad10127ac948
FelipeS3 <Felipexy50@gmail.com>
2025-04-03 13:39:45 -0300

    DTOs

--- BloodDonation.API/Models/DonorInputViewModel.cs
+++ BloodDonation.API/Models/DonorInputViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace BloodDonation.API.Models;
+﻿using BloodDonation.API.Entities;
+using BloodDonation.API.Eum;
 
-public class DonorInputViewModel
+namespace BloodDonation.API.Models;
+
+public record DonorInputViewModel
 {
-    
+    public string FullName { get;  set; }
+    public string Email { get;  set; }
+    public DateTime BirthDate { get;  set; }
+    public Gender Gender { get; set; }
+    public double Weight { get; set; }
+    public string BloodType { get; set; }
+    public string RhFactor { get; set; }
 }
---
+++ BloodDonation.API/Models/DonorUpdateInputModel.cs
@@ -0,0 +1,11 @@
+﻿using BloodDonation.API.Entities;
+using BloodDonation.API.Eum;
+
+namespace BloodDonation.API.Models;
+
+public class DonorUpdateInputModel
+{
+    public string? FullName { get; private set; }
+    public string? Email { get; private set; }
+    public double Weight { get; private set; }
+}
