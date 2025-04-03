0c9e6ac227d29257c3a1645fb7608fcb765c934b
FelipeS3 <Felipexy50@gmail.com>
2025-04-03 14:56:52 -0300

    fixing issues in the constructor

--- BloodDonation.API/Entities/Address .cs
+++ BloodDonation.API/Entities/Address .cs
@@ -2,18 +2,19 @@
 
 public class Address : BaseEntity
 {
-    public Address(string street, string city, string state, string zipCode, Donor donor)
+    public Address(string street, string city, string state, string zipCode, int donorId)
     {
         Street = street;
         City = city;
         State = state;
         ZipCode = zipCode;
-        Donor = donor;
+        DonorId = donorId;
     }
 
     public string Street { get; private set; }
     public string City { get; private set; }
     public string State { get; private set; }
     public string ZipCode { get; private set; }
+    public int DonorId { get; private set; }
     public Donor Donor { get; private set; }
 }
