67d9b54d9427e1df025b4517d03d7cf7b1712aea
FelipeS3 <Felipexy50@gmail.com>
2025-04-03 14:31:00 -0300

    adding dto to create donation

---
+++ BloodDonation.API/Models/CreateDonationInputModel.cs
@@ -0,0 +1,17 @@
+﻿using BloodDonation.API.Entities;
+
+namespace BloodDonation.API.Models;
+
+public class CreateDonationInputModel
+{
+    public CreateDonationInputModel(int donorId, int volumeInMl)
+    {
+        DonorId = donorId;
+        VolumeInML = volumeInMl;
+    }
+
+    public int DonorId { get; private set; }
+    public int VolumeInML { get; private set; }
+
+    public Donation ToEntity() => new(DonorId, VolumeInML);
+}
