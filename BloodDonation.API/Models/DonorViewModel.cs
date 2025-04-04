9cdd64d095e48987db4e217012b4c5af5790aece
FelipeS3 <Felipexy50@gmail.com>
2025-04-04 14:38:57 -0300

    update

---
+++ BloodDonation.API/Models/DonorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BloodDonation.API.Entities;
+
+namespace BloodDonation.API.Models;
+
+public class DonorViewModel
+{
+    public DonorViewModel(int id, string fullName, string email, int donationsCount)
+    {
+        Id = id;
+        FullName = fullName;
+        Email = email;
+        DonationCount = donationsCount;
+    }
+    public int Id { get; private set; }
+    public string FullName { get; private set; }
+    public string Email { get; private set; }
+    public int DonationCount { get; private set; }
+
+    public static DonorViewModel FromEntity(Donor donor) => new(donor.Id, donor.FullName, donor.Email,donor.Donations.Count);
+}
