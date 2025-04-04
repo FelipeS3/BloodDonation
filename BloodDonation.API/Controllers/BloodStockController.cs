408d375d7784d73011318107062983bff82dbb84
FelipeS3 <Felipexy50@gmail.com>
2025-04-04 15:25:36 -0300

    starting bloodcontroller

---
+++ BloodDonation.API/Controllers/BloodStockController.cs
@@ -0,0 +1,17 @@
+﻿using BloodDonation.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.API.Controllers;
+
+[ApiController]
+[Route("[controller]/api")]
+public class BloodStockController : ControllerBase
+{
+    private readonly BloodDonationDbContext _context;
+    public BloodStockController(BloodDonationDbContext context)
+    {
+        _context = context;
+    }
+
+
+}
