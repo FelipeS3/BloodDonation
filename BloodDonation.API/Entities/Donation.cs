fc2998dafa38d659d28eecfb558fc5023bc9827d
FelipeS3 <Felipexy50@gmail.com>
2025-04-04 11:46:56 -0300

    update donation

--- BloodDonation.API/Entities/Donation.cs
+++ BloodDonation.API/Entities/Donation.cs
@@ -10,18 +10,12 @@ public class Donation : BaseEntity
         VolumeInML = volumeInML;
         DonationDate = DateTime.UtcNow;
 
-        ValidateVolume();
+        if (volumeInML is < 420 or > 470)
+            throw new ArgumentException("Blood donation volume must be between 420ml and 470ml");
     }
     public int VolumeInML { get; private set; }
     public DateTime DonationDate { get; private set; }
     public int DonorId { get; private set; }
     public Donor Donor { get; private set; }
 
-    private void ValidateVolume()
-    {
-        if (VolumeInML is < 420 or > 470)
-        {
-            throw new ArgumentException("Blood donation volume must be between 420ml and 470ml");
-        }
-    }
 } 
