240a16c3a6ac787f3c89d14375043de9acba9bd7
FelipeS3 <Felipexy50@gmail.com>
2025-04-04 11:41:12 -0300

    adding blood bank entity

--- BloodDonation.API/Entities/BloodStock.cs
+++ BloodDonation.API/Entities/BloodStock.cs
@@ -2,6 +2,15 @@
 
 public class BloodStock : BaseEntity
 {
-    
+    public BloodStock(string bloodType, string rhFactor, int volumeInMl)
+    {
+        BloodType = bloodType;
+        RhFactor = rhFactor;
+        VolumeInML = volumeInMl;
+    }
+
+    public string BloodType { get; private set; }
+    public string RhFactor { get; private set; }
+    public int VolumeInML { get; private set; }
 }
 
