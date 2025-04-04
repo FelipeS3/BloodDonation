b500290c6edd23c440285516b198294b7f15e390
FelipeS3 <Felipexy50@gmail.com>
2025-04-04 14:39:02 -0300

    update

--- BloodDonation.API/Models/DonorUpdateInputModel.cs
+++ BloodDonation.API/Models/DonorUpdateInputModel.cs
@@ -6,8 +6,16 @@ namespace BloodDonation.API.Models;
 
 public record DonorUpdateInputModel
 {
-    public string? FullName { get; private set; }
+    public DonorUpdateInputModel(string fullName, string email, double weight)
+    {
+        FullName = fullName;
+        Email = email;
+        Weight = weight;
+    }
+    public string FullName { get; private set; }
     [EmailAddress]
-    public string? Email { get; private set; }
+    public string Email { get; private set; }
     public double Weight { get; private set; }
+
+    
 }
