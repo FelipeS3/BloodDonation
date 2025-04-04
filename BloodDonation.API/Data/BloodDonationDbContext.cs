2cf49c3c3920a1ea5f3fa72997ff9910b45b00d1
FelipeS3 <Felipexy50@gmail.com>
2025-04-04 13:41:51 -0300

    implementing conf for database

--- BloodDonation.API/Data/BloodDonationDbContext.cs
+++ BloodDonation.API/Data/BloodDonationDbContext.cs
@@ -8,31 +8,43 @@ public class BloodDonationDbContext : DbContext
     public BloodDonationDbContext(DbContextOptions<BloodDonationDbContext> options) : base(options)
     { }
 
-    public DbSet<Donor> Donors { get; private set; }
-    public DbSet<Donation> Donations { get; private set; }
-    public DbSet<BloodStock> BloodStocks { get; private set; }
-    public DbSet<Address> Addresses { get; private set; }
+    public DbSet<Donor> Donors { get; set; }
+    public DbSet<Donation> Donations { get; set; }
+    public DbSet<BloodStock> BloodStocks { get; set; }
+    public DbSet<Address> Addresses { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.Entity<Donor>(e=>
         {
-            
+            e.HasKey(d => d.Id);
+
+            e.HasMany(d => d.Donations)
+                .WithOne(d => d.Donor)
+                .HasForeignKey(d => d.DonorId);
         });
 
-        builder.Entity<Donor>(e =>
+        builder.Entity<Donation>(e =>
         {
+            e.HasKey(d => d.Id);
 
+            e.HasOne(d => d.Donor)
+                .WithMany(d => d.Donations)
+                .HasForeignKey(d => d.DonorId);
         });
 
-        builder.Entity<Donor>(e =>
+        builder.Entity<BloodStock>(e =>
         {
-
+            e.HasKey(b => b.Id);
         });
 
-        builder.Entity<Donor>(e =>
+        builder.Entity<Address>(e =>
         {
+            e.HasKey(a => a.Id);
 
+            e.HasOne(b => b.Donor)
+                .WithOne(b => b.Address)
+                .HasForeignKey<Address>(a => a.DonorId);
         });
     }
 }
