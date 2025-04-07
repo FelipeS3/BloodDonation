f6079e284bcf8bbb360be109c6ca67542c499c20
FelipeS3 <Felipexy50@gmail.com>
2025-04-07 15:31:45 -0300

    update

--- BloodDonation.API/Controllers/BloodStockController.cs
+++ BloodDonation.API/Controllers/BloodStockController.cs
@@ -23,5 +23,10 @@ public class BloodStockController : ControllerBase
         return Ok(stocks);
     }
 
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Created();
+    }
 
 }
