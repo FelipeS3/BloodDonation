d23218a9513b52ea6d2ad0731c5a01b92b193228
FelipeS3 <Felipexy50@gmail.com>
2025-04-15 15:37:47 -0300

    feat(application): implementing ResultViewModel pattern

--- BloodDonation.Application/Models/ResultViewModel.cs
+++ BloodDonation.Application/Models/ResultViewModel.cs
@@ -14,12 +14,16 @@ public class ResultViewModel
     public string Message { get; private set; }
 
 }
-
 public class ResultViewModel<T> : ResultViewModel
 {
     public ResultViewModel(T? data, bool isSuccess = true, string message = "") : base(isSuccess, message)
     {
         Data = data;
     }
+
     public T? Data { get; private set; }
+
+    public static ResultViewModel<T> Success(T data) => new(default, true, "");
+
+    public static ResultViewModel<T> Error(string message) => new(default, false, message);
 }
