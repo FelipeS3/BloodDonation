f39198234dc1d129e62fed2e1b634fcdb0ab604e
FelipeS3 <Felipexy50@gmail.com>
2025-04-16 12:51:34 -0300

    feat(interfaces): add IDonationService interface for managing donation operations

---
+++ BloodDonation.Application/Services/IDonationService.cs
@@ -0,0 +1,10 @@
+﻿using BloodDonation.Application.Models;
+
+namespace BloodDonation.Application.Services;
+
+public interface IDonationService
+{
+    ResultViewModel<List<DonationViewModel>> GetAll();
+    ResultViewModel<DonationDetailsViewModel> Get(int id);
+    ResultViewModel<int> Insert(CreateDonationInputModel input);
+}
