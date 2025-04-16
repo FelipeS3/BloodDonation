6bddf3b0e1fa3b95967046d4a61b77d7da0a17b4
FelipeS3 <Felipexy50@gmail.com>
2025-04-16 12:49:56 -0300

    feat(api): add ReportsController to handle donation summary report

---
+++ BloodDonation.API/Controllers/ReportsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.API.Controllers;
+
+[ApiController]
+[Route("[controller]/api")]
+public class ReportsController : ControllerBase
+{
+    
+}
