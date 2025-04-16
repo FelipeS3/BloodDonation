a68f853f008203746d02dc0450d3ff064ef96d7c
FelipeS3 <Felipexy50@gmail.com>
2025-04-16 12:50:04 -0300

    feat(model): create BloodStockViewModel with static FromEntity method

---
+++ BloodDonation.Application/Models/BloodStockViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BloodDonation.Core.Entities;
+
+namespace BloodDonation.Application.Models;
+
+public class BloodStockViewModel
+{
+    public BloodStockViewModel(string bloodType, string rhFactor, int volumeInMl)
+    {
+        BloodType = bloodType;
+        RhFactor = rhFactor;
+        VolumeInMl = volumeInMl;
+    }
+
+    public string BloodType { get; private set; }
+    public string RhFactor { get; private set; }
+    public int VolumeInMl { get; private set; }
+
+    public static BloodStockViewModel FromEntity(BloodStock stock) =>
+        new(stock.BloodType, stock.RhFactor, stock.VolumeInMl);
+}
