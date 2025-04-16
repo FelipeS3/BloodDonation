62a602fab65ce602072f13491a3d20d309b536d2
FelipeS3 <Felipexy50@gmail.com>
2025-04-16 12:51:19 -0300

    feat(interfaces): define IBloodStockService interface for blood stock operations

---
+++ BloodDonation.Application/Services/IBloodStockService.cs
@@ -0,0 +1,9 @@
+﻿using BloodDonation.Application.Models;
+
+namespace BloodDonation.Application.Services;
+
+public interface IBloodStockService
+{
+    ResultViewModel<List<BloodStockViewModel>> GetAll();
+    ResultViewModel<BloodStockViewModel> Get(string? blood = "", string? rhFactor = "");
+}
