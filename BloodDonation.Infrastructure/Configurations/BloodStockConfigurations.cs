60772b8dbd421b9bd67775f4ebe873d997b3e168
FelipeS3 <Felipexy50@gmail.com>
2025-04-30 13:52:55 -0300

    refactor: move BloodStock mapping to BloodStockConfiguration class

---
+++ BloodDonation.Infrastructure/Configurations/BloodStockConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using BloodDonation.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodDonation.Infrastructure.Configurations;
+
+public class BloodStockConfigurations : IEntityTypeConfiguration<BloodStock>
+{
+    public void Configure(EntityTypeBuilder<BloodStock> builder)
+    {
+        builder.HasKey(b => b.Id);
+    }
+}
