26f904a166dadc24afd55b9f5539374e973a25d9
FelipeS3 <Felipexy50@gmail.com>
2025-04-30 13:52:45 -0300

    refactor: move Address mapping to AddressConfiguration class

---
+++ BloodDonation.Infrastructure/Configurations/AddressConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using BloodDonation.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodDonation.Infrastructure.Configurations;
+
+public class AddressConfigurations : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder.HasOne(b => b.Donor)
+            .WithOne(b => b.Address)
+            .HasForeignKey<Address>(a => a.DonorId);
+    }
+}
