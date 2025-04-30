b375733f42e6537f5cef075aa00922d9cd2938b7
FelipeS3 <Felipexy50@gmail.com>
2025-04-30 13:52:25 -0300

    refactor: move Donor mapping to DonorConfiguration class

---
+++ BloodDonation.Infrastructure/Configurations/DonorConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using BloodDonation.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodDonation.Infrastructure.Configurations;
+
+public class DonorConfigurations : IEntityTypeConfiguration<Donor>
+{
+    public void Configure(EntityTypeBuilder<Donor> builder)
+    {
+        builder.HasKey(d => d.Id);
+
+        builder.HasMany(d => d.Donations)
+            .WithOne(d => d.Donor)
+            .HasForeignKey(d => d.DonorId);
+    }
+}
