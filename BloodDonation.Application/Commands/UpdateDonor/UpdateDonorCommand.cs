c3872fb735804272ae763b54b83283973c987916
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:29:32 -0300

    feat(CQRS): add UpdateDonor Command

--- BloodDonation.Application/Commands/UpdateDonor/UpdateDonorCommand.cs
+++ BloodDonation.Application/Commands/UpdateDonor/UpdateDonorCommand.cs
@@ -5,13 +5,8 @@ namespace BloodDonation.Application.Commands.UpdateDonor;
 
 public class UpdateDonorCommand : IRequest<ResultViewModel>
 {
-    public UpdateDonorCommand(int id)
-    {
-        DonorId = id;
-    }
-    public int DonorId { get; private set; }
+    public int DonorId { get; set; }
     public string FullName { get; set; }
     public string Email { get; set; }
     public double Weight { get; set; }
-
 }
