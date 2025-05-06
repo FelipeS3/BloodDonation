4b8893c9bf715b1b745d33e8176602905295fc0b
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 09:59:01 -0300

    refactor: streamline donor update process

---
+++ BloodDonation.Application/Commands/UpdateDonor/UpdateDonorCommand.cs
@@ -0,0 +1,17 @@
+﻿using BloodDonation.Application.Models;
+using MediatR;
+
+namespace BloodDonation.Application.Commands.UpdateDonor;
+
+public class UpdateDonorCommand : IRequest<ResultViewModel>
+{
+    public UpdateDonorCommand(int id)
+    {
+        DonorId = id;
+    }
+    public int DonorId { get; private set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public double Weight { get; set; }
+
+}
---
+++ BloodDonation.Application/Commands/UpdateDonor/UpdateDonorHandler.cs
@@ -0,0 +1,36 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace BloodDonation.Application.Commands.UpdateDonor;
+
+public class UpdateDonorHandler : IRequestHandler<UpdateDonorCommand, ResultViewModel>
+{
+    private readonly BloodDonationDbContext _context;
+    public UpdateDonorHandler(BloodDonationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ResultViewModel> Handle(UpdateDonorCommand request, CancellationToken cancellationToken)
+    {
+        var donor = await _context.Donors.FindAsync(request.DonorId);
+
+        if (donor == null)
+        {
+            return new ResultViewModel(false, "No donors registered yet");
+        }
+
+        if (await _context.Donors.AnyAsync(d => d.Email == request.Email && d.Id != donor.Id))
+        {
+            return new ResultViewModel(false, "Email already exist.");
+        }
+
+        donor.Update(request.FullName, request.Email, request.Weight);
+        await _context.SaveChangesAsync();
+
+        return new ResultViewModel(true, "Updated!");
+    }
+}
