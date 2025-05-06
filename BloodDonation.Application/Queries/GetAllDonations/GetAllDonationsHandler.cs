5c77c0804ae3d37d631e2c19c533cbb80ba7e2cf
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:30:15 -0300

    feat(CQRS): add GetAll Donations Handler

---
+++ BloodDonation.Application/Queries/GetAllDonations/GetAllDonationsHandler.cs
@@ -0,0 +1,31 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BloodDonation.Application.Queries.GetAllDonations;
+
+public class GetAllDonationsHandler : IRequestHandler<GetAllDonationsQuery, ResultViewModel<List<DonationViewModel>>>
+{
+    private readonly BloodDonationDbContext _context;
+
+    public GetAllDonationsHandler(BloodDonationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ResultViewModel<List<DonationViewModel>>> Handle(GetAllDonationsQuery request, CancellationToken cancellationToken)
+    {
+        var donation = await _context.Donations.Include(d => d.Donor).ToListAsync();
+
+        if (donation.IsNullOrEmpty())
+        {
+            return ResultViewModel<List<DonationViewModel>>.Error("No donations registered yet");
+        }
+
+        var donationsView = donation.Select(d => DonationViewModel.FromEntity(d)).ToList();
+
+        return ResultViewModel<List<DonationViewModel>>.Success(donationsView);
+    }
+}
