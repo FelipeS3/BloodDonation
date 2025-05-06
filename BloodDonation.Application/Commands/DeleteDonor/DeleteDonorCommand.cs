e5ccc57bc1fec9520537c6f0a92c72a9dde02bf8
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 09:59:41 -0300

    refactor: enhance donor deletion logic

---
+++ BloodDonation.Application/Commands/DeleteDonor/DeleteDonorCommand.cs
@@ -0,0 +1,13 @@
+﻿using BloodDonation.Application.Models;
+using MediatR;
+
+namespace BloodDonation.Application.Commands.DeleteDonor;
+
+public class DeleteDonorCommand : IRequest<ResultViewModel>
+{
+    public DeleteDonorCommand(int id)
+    {
+        Id = id;
+    }
+    public int Id { get; set; }
+}
---
+++ BloodDonation.Application/Commands/DeleteDonor/DeleteDonorHandler.cs
@@ -0,0 +1,31 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Application.Services;
+using BloodDonation.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonation.Application.Commands.DeleteDonor;
+
+public class DeleteDonorHandler : IRequestHandler<DeleteDonorCommand, ResultViewModel>
+{
+    private readonly BloodDonationDbContext _context;
+    public DeleteDonorHandler(BloodDonationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ResultViewModel> Handle(DeleteDonorCommand request, CancellationToken cancellationToken)
+    {
+        var donor = await _context.Donors.SingleOrDefaultAsync(d=>d.Id == request.Id);
+
+        if (donor == null)
+        {
+            return new ResultViewModel(false, "No donors registered yet");
+        }
+
+        _context.Donors.Remove(donor);
+        await _context.SaveChangesAsync();
+
+        return new ResultViewModel(true, "Deleted");
+    }
+}
