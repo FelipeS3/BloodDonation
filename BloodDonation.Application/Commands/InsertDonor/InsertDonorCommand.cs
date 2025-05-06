990d2d49e3d5568738077b02991f3c8c708e87bd
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 09:59:11 -0300

    refactor: optimize donor insertion flow

---
+++ BloodDonation.Application/Commands/InsertDonor/InsertDonorCommand.cs
@@ -0,0 +1,19 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Core.Entities;
+using BloodDonation.Core.Enum;
+using MediatR;
+
+namespace BloodDonation.Application.Commands.InsertDonor;
+
+public class InsertDonorCommand : IRequest<ResultViewModel<int>>
+{
+    public string FullName { get;  set; }
+    public string Email { get;  set; }
+    public DateTime BirthDate { get;  set; }
+    public Gender Gender { get;  set; }
+    public double Weight { get;  set; }
+    public string BloodType { get;  set; }
+    public string RhFactor { get;  set; }
+
+    public Donor ToEntity() => new(FullName, Email, BirthDate, Gender, Weight, BloodType, RhFactor);
+}
---
+++ BloodDonation.Application/Commands/InsertDonor/InsertDonorHandler.cs
@@ -0,0 +1,35 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonation.Application.Commands.InsertDonor;
+
+public class InsertDonorHandler : IRequestHandler<InsertDonorCommand, ResultViewModel<int>>
+{
+    private readonly BloodDonationDbContext _context;
+    public InsertDonorHandler(BloodDonationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ResultViewModel<int>> Handle(InsertDonorCommand request, CancellationToken cancellationToken)
+    {
+        var donor = request.ToEntity();
+
+        if (await _context.Donors.AnyAsync(d => d.Email == donor.Email))
+        {
+            return ResultViewModel<int>.Error("Email already exist.");
+        }
+
+        if (string.IsNullOrEmpty(donor.FullName))
+        {
+            return ResultViewModel<int>.Error("Full name cannot be empty.");
+        }
+
+        await _context.Donors.AddAsync(donor);
+        await _context.SaveChangesAsync();
+
+        return ResultViewModel<int>.Success(donor.Id);
+    }
+}
