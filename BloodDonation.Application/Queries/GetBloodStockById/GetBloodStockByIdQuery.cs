6bdab36d9d0f2964cb1015d11d41105a4fba05e5
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:31:26 -0300

    feat(CQRS): add GetbyId BloodStock Query

---
+++ BloodDonation.Application/Queries/GetBloodStockById/GetBloodStockByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BloodDonation.Application.Models;
+using MediatR;
+
+namespace BloodDonation.Application.Queries.GetBloodStockById;
+
+public class GetBloodStockByIdQuery : IRequest<ResultViewModel<BloodStockViewModel>>
+{
+    public int Id { get; set; }
+}
