b7cbbd7a691ade764d32863b8b7d79d209c5ccb1
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:29:09 -0300

    feat(CQRS): add InsertDonor Handler

--- BloodDonation.Application/Commands/InsertDonor/InsertDonorHandler.cs
+++ BloodDonation.Application/Commands/InsertDonor/InsertDonorHandler.cs
@@ -15,21 +15,31 @@ public class InsertDonorHandler : IRequestHandler<InsertDonorCommand, ResultView
 
     public async Task<ResultViewModel<int>> Handle(InsertDonorCommand request, CancellationToken cancellationToken)
     {
-        var donor = request.ToEntity();
-
-        if (await _context.Donors.AnyAsync(d => d.Email == donor.Email))
+        try
         {
-            return ResultViewModel<int>.Error("Email already exist.");
+
+            var donor = request.ToEntity();
+
+            if (await _context.Donors.AnyAsync(d => d.Email == donor.Email))
+            {
+                return ResultViewModel<int>.Error("Email already exist.");
+            }
+
+            if (string.IsNullOrEmpty(donor.FullName))
+            {
+                return ResultViewModel<int>.Error("Full name cannot be empty.");
+            }
+
+            await _context.Donors.AddAsync(donor, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return ResultViewModel<int>.Success(donor.Id);
         }
 
-        if (string.IsNullOrEmpty(donor.FullName))
+
+        catch (Exception ex)
         {
-            return ResultViewModel<int>.Error("Full name cannot be empty.");
+            return ResultViewModel<int>.Error($"Erro interno: {ex.InnerException?.Message ?? ex.Message}");
         }
-
-        await _context.Donors.AddAsync(donor);
-        await _context.SaveChangesAsync();
-
-        return ResultViewModel<int>.Success(donor.Id);
     }
 }
