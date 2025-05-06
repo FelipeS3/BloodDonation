ea22485043756f570e805bfb3396da7b98ae064a
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:31:46 -0300

    feat(CQRS): add GetbyId Donations Query

---
+++ BloodDonation.Application/Queries/GetDonationById/GetDonationsByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using BloodDonation.Application.Models;
+using MediatR;
+
+namespace BloodDonation.Application.Queries.GetDonationById;
+
+public class GetDonationsByIdQuery : IRequest<ResultViewModel<DonationDetailsViewModel>>
+{
+    public GetDonationsByIdQuery(int id)
+    {
+        Id = id;
+    }
+    public int Id { get; set; }
+}
