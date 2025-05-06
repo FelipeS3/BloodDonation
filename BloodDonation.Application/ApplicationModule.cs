29ba2b33b6d6ed2ac08d56759b061455bd007e9d
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:34:04 -0300

    refactor(module): setup dependency injection for CQRS

--- BloodDonation.Application/ApplicationModule.cs
+++ BloodDonation.Application/ApplicationModule.cs
@@ -1,5 +1,5 @@
-﻿using BloodDonation.Application.Services;
-using BloodDonation.Core.Entities;
+﻿using BloodDonation.Application.Commands.InsertDonor;
+using BloodDonation.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BloodDonation.Application;
@@ -9,6 +9,7 @@ public static class ApplicationModule
     public static IServiceCollection AddApplication(this IServiceCollection service)
     {
         service
+            .AddHandlers()
             .AddServices();
 
         return service;
@@ -19,6 +20,13 @@ public static class ApplicationModule
         service.AddScoped<IDonorService, DonorService>();
         service.AddScoped<IDonationService, DonationService>();
         service.AddScoped<IBloodStockService, BloodStockService>();
+
+        return service;
+    }
+
+    private static IServiceCollection AddHandlers(this IServiceCollection service)
+    {
+        service.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<InsertDonorCommand>());
 
         return service;
     }
