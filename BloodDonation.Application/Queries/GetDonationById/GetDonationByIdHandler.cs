7846bb2a68742a24e3b69a9526af3d314bdeda8c
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:31:53 -0300

    feat(CQRS): add GetbyId BloodStock Handler

---
+++ BloodDonation.Application/Queries/GetDonationById/GetDonationByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonation.Application.Queries.GetDonationById;
+
+public class GetDonationByIdHandler : IRequestHandler<GetDonationsByIdQuery, ResultViewModel<DonationDetailsViewModel>>
+{
+    private readonly BloodDonationDbContext _context;
+    public GetDonationByIdHandler(BloodDonationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ResultViewModel<DonationDetailsViewModel>> Handle(GetDonationsByIdQuery request, CancellationToken cancellationToken)
+    {
+        var donation = await _context.Donations.Include(d => d.Donor).SingleOrDefaultAsync(d => d.Id == request.Id);
+
+        if (donation == null)
+        {
+            return ResultViewModel<DonationDetailsViewModel>.Error("No donations registered yet");
+        }
+
+        var model = DonationDetailsViewModel.FromEntity(donation);
+
+        return ResultViewModel<DonationDetailsViewModel>.Success(model);
+    }
+}
