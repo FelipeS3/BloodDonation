aacede2e10cf28697254b4b34143efcc7d4607ec
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:32:26 -0300

    feat(CQRS): add GetbyId Donors Handler

---
+++ BloodDonation.Application/Queries/GetDonorById/GetDonorByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonation.Application.Queries.GetDonorById;
+
+public class GetDonorByIdHandler : IRequestHandler<GetDonorByIdQuery, ResultViewModel<DonorDetailsViewModel>>
+{
+    private readonly BloodDonationDbContext _context;
+    public GetDonorByIdHandler(BloodDonationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ResultViewModel<DonorDetailsViewModel>> Handle(GetDonorByIdQuery request, CancellationToken cancellationToken)
+    {
+        var donor = await _context.Donors.Include(d => d.Donations).FirstOrDefaultAsync(d => d.Id == request.Id);
+
+        if (donor == null)
+        {
+            return ResultViewModel<DonorDetailsViewModel>.Error("Donor Not Found");
+        }
+
+        var model = DonorDetailsViewModel.FromEntity(donor);
+
+        return ResultViewModel<DonorDetailsViewModel>.Success(model);
+    }
+}
