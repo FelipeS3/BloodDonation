5f6d739f9cd490ea5c34f314c4a260ae1c63e076
FelipeS3 <Felipexy50@gmail.com>
2025-05-06 14:28:31 -0300

    feat(CQRS): add InsertDonation command

---
+++ BloodDonation.Application/Commands/InsertDonation/InsertDonationCommand.cs
@@ -0,0 +1,15 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Core.Entities;
+using MediatR;
+
+namespace BloodDonation.Application.Commands.InsertDonation;
+
+public class InsertDonationCommand : IRequest<ResultViewModel<int>>
+{
+    public InsertDonationCommand() { }
+
+    public int DonorId { get; set; }
+    public int VolumeInML { get; set; }
+
+    public Donation ToEntity() => new(DonorId, VolumeInML);
+}
