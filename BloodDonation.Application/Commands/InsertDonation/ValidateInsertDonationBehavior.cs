2d21bfbf4f310c0d820c78880fcb5ccfabe63165
FelipeS3 <Felipexy50@gmail.com>
2025-05-07 13:31:51 -0300

    feat: Add validation to InsertDonationCommand

---
+++ BloodDonation.Application/Commands/InsertDonation/ValidateInsertDonationBehavior.cs
@@ -0,0 +1,26 @@
+﻿using BloodDonation.Application.Models;
+using BloodDonation.Infrastructure.Persistence;
+using MediatR;
+
+namespace BloodDonation.Application.Commands.InsertDonation;
+
+public class ValidateInsertDonationBehavior : IPipelineBehavior<InsertDonationCommand, ResultViewModel<int>>
+{
+    private readonly BloodDonationDbContext _context;
+    public ValidateInsertDonationBehavior(BloodDonationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ResultViewModel<int>> Handle(InsertDonationCommand request, RequestHandlerDelegate<ResultViewModel<int>> next, CancellationToken cancellationToken)
+    {
+        var donorExists = _context.Donors.Any(d => d.Id == request.DonorId);
+
+        if (!donorExists)
+        {
+            return ResultViewModel<int>.Error("Invalid donor data.");
+        }
+
+        return await next();
+    }
+}
