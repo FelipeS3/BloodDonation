edf8a1c9a9331caf3c0a19192e2f5fe63ef83ef4
FelipeS3 <Felipexy50@gmail.com>
2025-05-08 11:16:53 -0300

    feat: add ValidationFilter to handle model state errors

---
+++ BloodDonation.Application/Filters/ValidationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BloodDonation.Application.Filters;
+
+public class ValidationFilter : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            var messages = context.ModelState
+                .SelectMany(x => x.Value.Errors)
+                .Select(x => x.ErrorMessage)
+                .ToList();
+
+            context.Result = new BadRequestObjectResult(messages);
+        }
+    }
+}
